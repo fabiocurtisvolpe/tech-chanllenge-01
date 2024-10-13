6d92925e42806c7a6302bc52f4b23927a40eec22
Fabio Curtis Volpe <curtis.fabio@outlook.com>
2024-10-13 08:32:39 -0300

    First commit

---
+++ src/app/components/list-transaction.tsx
@@ -0,0 +1,237 @@
+import React from "react";
+import { Transaction } from "../interfaces/transaction.interface";
+import NewTransaction from "./new-transaction";
+import Dialog from "./dialog";
+
+class ListTransaction extends React.Component<{ transations?: Transaction[], refreshBalance?: any }> {
+
+    private tituloNewTransaction: string = "Nova Transação";
+
+    private transation: Transaction | undefined;
+    private transations: Transaction[] = [];
+
+    private isViewAllTransaction: boolean = false;
+    private viewAllTransaction: boolean = false;
+
+    private indexTransation?: number = undefined;
+
+    private classViewAllTransaction: string = "bi bi-arrow-bar-down";
+
+    constructor(props: {}) {
+        super(props);
+
+        this.state = {
+            isModalOpen: false,
+            isModalDialogOpen: false
+        }
+    }
+
+    componentDidMount() {
+        this.onInit();
+    }
+
+    private onInit = () => {
+
+        if (this.props.transations) {
+            this.transations = this.props.transations;
+            this.setState({});
+        }
+    }
+
+    private addCollapse = (index: number): string => {
+        return ((index > 2) && (!this.isViewAllTransaction)) ? "collapse" : "";
+    }
+
+    private formatAmount = (amount: number): string => {
+        amount = amount >= 0 ? amount : (-1) * amount;
+        return amount.toLocaleString('pt-BR', {
+            style: 'currency',
+            currency: 'BRL',
+        });
+    };
+
+    private handleViewAllTransaction = (): void => {
+        this.isViewAllTransaction = !this.isViewAllTransaction;
+        this.classViewAllTransaction = this.viewAllTransaction ? "bi bi-arrow-bar-down" : "bi bi-arrow-bar-up";
+        this.setState({});
+    }
+
+    private handleNewTransaction = () => {
+        this.tituloNewTransaction = "Nova Transação";
+        this.transation = undefined;
+        this.setState({
+            isModalOpen: true,
+            isModalDialogOpen: false
+        });
+    }
+
+    private handleCloseTransaction = () => {
+        this.setState({
+            isModalOpen: false
+        })
+    };
+
+    private handleDeteleTransaction = () => {
+
+        if (this.indexTransation != undefined) {
+
+            this.transations.splice(this.indexTransation, 1);
+            this.transations.sort((a, b) => b.date.getTime() - a.date.getTime());
+
+            let sum: number = 0;
+            this.transations.forEach((t: Transaction) => {
+                sum += t.amount;
+            });
+
+            this.props.refreshBalance(sum);
+
+            this.handleCloseDeleteTransaction();
+            this.setState({});
+        }
+    }
+
+    private handleCloseDeleteTransaction = () => {
+        this.setState({
+            isModalDialogOpen: false
+        })
+
+        this.indexTransation = undefined;
+    };
+
+    private setNewTransation = (t: Transaction) => {
+
+        if (t) {
+
+            this.transations.push(t);
+            this.transations.sort((a, b) => b.date.getTime() - a.date.getTime());
+
+            let sum: number = 0;
+            this.transations.forEach((t: Transaction) => {
+                sum += t.amount;
+            });
+
+            this.props.refreshBalance(sum);
+            this.setState({});
+        }
+    }
+
+    private setEditTransation = (t: Transaction) => {
+
+        this.handleDeteleTransaction();
+        setTimeout(() => {
+            this.setNewTransation(t);
+        }, 250)
+        
+    }
+
+    private editTransation = (t: Transaction, index: number) => {
+        this.transation = t;
+        this.indexTransation = index;
+        this.tituloNewTransaction = "Editar Transação";
+        this.setState({
+            isModalOpen: true,
+            isModalDialogOpen: false
+        });
+    }
+
+    private deleteTransation = (index: number) => {
+
+        this.indexTransation = index;
+        this.setState({
+            isModalOpen: false,
+            isModalDialogOpen: true
+        })
+    }
+
+    render() {
+
+        return (
+            <div className="panel panel-default rounded-lg shadow-lg">
+
+                <div className="d-flex justify-content-end">
+                    <button type="button" className="btn btn-info m-lg-4 text-end" data-toggle="modal" data-target="#myModal" onClick={this.handleNewTransaction}>Nova
+                        Transação
+                    </button>
+                </div>
+
+                <div className="panel-body m-lg-4">
+                    <div className="table-responsive">
+                        <table className="table table-hover">
+                            <caption>
+                                <button type="button" className="btn btn-outline-secondary btn-sm m-lg-2"
+                                    data-toggle="collapse" data-target="#collapseTr" aria-controls="collapseTr"
+                                    onClick={this.handleViewAllTransaction} title="Extrato das últimas transações">
+                                    <i className={this.classViewAllTransaction}></i>
+                                </button>
+                            </caption>
+
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Data</th>
+                                    <th scope="col">Valor</th>
+                                    <th scope="col">Tipo</th>
+                                    <th scope="col">Ação</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                {this.transations.map((t, index) => (
+                                    <tr key={index} className={this.addCollapse(index)} id="collapseTr">
+                                        <td>{index + 1}</td>
+                                        <td>{t.dateFormated}</td>
+                                        <td className={t.classAmount}>
+                                            {this.formatAmount(t.amount)}
+                                        </td>
+                                        <td>{t.type}</td>
+                                        <td>
+                                            <button className="btn btn-success m-lg-1" title="Editar Transação" onClick={() => this.editTransation(t, index)}>
+                                                <i className="bi bi-vector-pen"></i>
+                                            </button>
+                                            <button className="btn btn-warning m-lg-1" title="Excluir Transação" onClick={() => this.deleteTransation(index)}>
+                                                <i className="bi bi-trash-fill"></i>
+                                            </button>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+
+                        </table>
+                    </div>
+                </div>
+
+                {this.state.isModalOpen ?
+                    <NewTransaction
+                        onClose={this.handleCloseTransaction}
+                        setNewTransation={this.setNewTransation}
+                        editTransation={this.setEditTransation}
+                        title={this.tituloNewTransaction}
+                        transation={this.transation}
+                        footer={
+                            <div>
+                                <button className="btn btn-secondary m-lg-1" onClick={this.handleCloseTransaction}>Fechar</button>
+                            </div>
+                        }
+                    /> : null
+                }
+
+                {this.state.isModalDialogOpen ?
+                    <Dialog
+                        onClose={this.handleCloseDeleteTransaction}
+                        title="Apagar a Transação"
+                        msg="Deseja apagar esta transação?"
+                        footer={
+                            <div>
+                                <button className="btn btn-secondary m-lg-1" onClick={this.handleCloseDeleteTransaction}>Fechar</button>
+                                <button className="btn btn-primary m-lg-1" onClick={this.handleDeteleTransaction}>Apagar</button>
+                            </div>
+                        }
+                    /> : null
+                }
+
+            </div>
+        );
+    };
+}
+
+export default ListTransaction;
---
+++ src/app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.min.css"
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Meu Banco Digital",
+  description: "Tech Chanllenge 1",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-br">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
